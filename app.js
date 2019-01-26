871da627a85cea4177d4e2efeb2e5c59280728a4
21x37 <tylermok@outlook.com>
2019-01-25 18:36:09 -0800

    Reorganized routes

--- app.js
+++ app.js
@@ -8,40 +8,22 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(cookieParser());
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-    const name = req.cookies.username
-    if (name) {
-        res.render('index', { name });
-    } else {
-        res.redirect('/hello')
-    }
-});
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards');
 
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
 
-
-app.post('/goodbye', (req, res) => {
-    const name = req.cookies.username;
-    res.clearCookie('username', name);
-    res.redirect('/hello');
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 })
 
-
-app.get('/cards', (req, res) => {
-    res.render('card', {prompt: 'Who is burried in Grant\'s tomb?'});
-});
-
-app.get('/hello', (req, res) => {
-    const name = req.cookies.username
-    if (!name) {
-        res.render('hello');
-    } else {
-        res.redirect('/');
-    }
-});
-
-app.post('/hello', (req, res) => {
-    res.cookie('username', req.body.username)
-    res.redirect('/')
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error', err);
 });
 
 
