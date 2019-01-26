f9cdab51a19973088b8bc81a02c9a83138761ec1
21x37 <tylermok@outlook.com>
2019-01-26 12:09:53 -0800

    Added styles, images

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const { data } = require('../data/flashcardData.json');
+const { cards } = data;
+
 
 router.get('/', (req, res) => {
-    res.render('card', {prompt: 'Who is burried in Grant\'s tomb?'});
+    const randomId = Math.floor(Math.random() * 5)
+    res.redirect(`/cards/${randomId}?side=question`)
+})
+
+router.get('/:id', (req, res) => {
+    const { side } = req.query;
+    const { id } = req.params;
+
+    if ( !side ) {
+        return res.redirect(`/cards/${id}?side=question`);
+    }
+
+    const name = req.cookies.username;
+    const text = cards[id][side];
+    const { hint } = cards[id];
+
+    
+    const templateData = {id, text, name, side};
+
+    if (side === 'question') {
+        templateData.hint = hint;
+        templateData.sideToShow = 'answer';
+        templateData.sideTo = 'Answer';
+    } else if ( side === 'answer') {
+        templateData.sideToShow = 'question';
+        templateData.sideTo = 'Question';
+    }
+
+    res.render('card', templateData);
 });
 
 module.exports = router;
